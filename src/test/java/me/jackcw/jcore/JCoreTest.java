1cfb040ca6cf5e2af5a44a1c3e75620a56be7862
Jack <cunninghamwhitej@gmail.com>
2026-09-09 15:45:35 +0100

    test: add JUnit setup

---
+++ src/test/java/me/jackcw/jcore/JCoreTest.java
@@ -0,0 +1,14 @@
+package me.jackcw.jcore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JCoreTest
+{
+    @Test
+    void testFrameworkSetup()
+    {
+        assertTrue(true);
+    }
+}
